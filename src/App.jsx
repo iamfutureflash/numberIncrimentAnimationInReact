bc137b22fac1a330f3fc93083b8ab49c088e0239
Puneet Vaishnav <puneet@coreteams.us>
2023-11-03 16:04:02 +0530

    added number increment animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(100);
+  useEffect(() => {
+    let currentNumber = 0;
+    const targetedNumber = 100;
+    const duration = 200;
+    const step = targetedNumber / duration;
 
+    console.log(currentNumber, targetedNumber, duration, step);
+
+    const interval = setInterval(() => {
+      if (currentNumber < targetedNumber) {
+        currentNumber += step;
+        setCount(Math.ceil(currentNumber));
+        console.log();
+      } else {
+        setCount(targetedNumber);
+        clearInterval(interval);
+        console.log();
+      }
+    }, 0);
+
+    return () => clearInterval(interval);
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="number-increment-animation">
+        <div className="number">{count}</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
